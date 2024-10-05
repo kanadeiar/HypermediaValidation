b1c262c6915af39479663e9337f00615da91b8e6
kanadeiar <kanadeiar@gmail.com>
2024-10-05 08:51:19 +0300

    Созданный и подготовленный начальный проект

---
+++ HypermediaValidation/HypermediaValidation/Models/UserWebModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace HypermediaValidation.Models;
+
+public class UserWebModel
+{
+    [Display(Name = "Имя")]
+    [Required(ErrorMessage = "Пожалуйста, введите свое имя")]
+    [StringLength(10, MinimumLength = 3, ErrorMessage = "Длинна имени должна быть от 3 до 10 символов")]
+    public string? Name { get; init; }
+
+    [Display(Name = "Дата рождения")]
+    [Required(ErrorMessage = "Пожалуйста, ведите дату рождения")]
+    [DataType(DataType.Date)]
+    public DateTime? BirthDay { get; init; }
+    
+    [Display(Name = "Согласен с правилами")]
+    public bool IsTermsAccepted { get; init; }
+
+    public void Validate(ModelStateDictionary state)
+    {
+        if (IsTermsAccepted == false)
+        {
+            state.AddModelError(nameof(IsTermsAccepted), 
+                "Необходимо подтвердить согласие с правилами");
+        }
+        if (BirthDay >= DateTime.Now)
+        {
+            state.AddModelError(nameof(BirthDay), 
+                "Дата рождения должна быть в прошлом");
+        }
+    }
+}
