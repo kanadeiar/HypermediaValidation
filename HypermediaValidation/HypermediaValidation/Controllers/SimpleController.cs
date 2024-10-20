00ebd03f2fb63a92109a5e98a59a47e4207872dc
kanadeiar <kanadeiar@gmail.com>
2024-10-20 16:28:03 +0300

    Простейшая гипермедийная валидация

---
+++ HypermediaValidation/HypermediaValidation/Controllers/SimpleController.cs
@@ -0,0 +1,23 @@
+﻿using Htmx;
+using HypermediaValidation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HypermediaValidation.Controllers;
+
+public class SimpleController : Controller
+{
+    public IActionResult Registration()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Registration(UserWebModel model)
+    {
+        model.Validate(ModelState);
+        if (Request.IsHtmx()) return PartialView("Partial/RegistrationPartial", model);
+        if (!ModelState.IsValid) return View(model);
+
+        return View("Complete");
+    }
+}
